1e93bbf09cb43c952d7aa82bc0a374d14295c39f
lcampit <leonardo932.campitelli@gmail.com>
2026-01-26 15:52:46 +0100

    feat: update ntfy adapter error handling

--- internal/ntfy/notify.go
+++ internal/ntfy/notify.go
@@ -11,7 +11,7 @@ func (a *ntfyAdapter) Notify(ctx context.Context, message string) error {
 	endpoint := fmt.Sprintf("%s/%s", a.ntfyHost, "cardwatcher-alerts-lc")
 	err := requests.URL(endpoint).Post().BodyBytes([]byte(message)).Fetch(ctx)
 	if err != nil {
-		return fmt.Errorf("error in ntfy adapter when sending message %s: %w", message, err)
+		return fmt.Errorf("sending message %s: %w", message, err)
 	}
 	return nil
 }
